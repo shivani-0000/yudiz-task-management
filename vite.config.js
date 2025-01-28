35c5553cc26a625971db40e27248a19e4f65ea2a
Shivani Patel <pshivani1610@gmail.com>
2025-01-28 15:46:30 +0530

    git pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/yudiz-task-management",
   resolve: {
     extensions: [".js", ".jsx"], // Add .jsx explicitly
   },
